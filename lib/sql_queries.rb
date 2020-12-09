ca0a938d515fc58ecef5c2e1b6d04806bb8ba901
Michael Fuget <michaelfuget@gmail.com>
2020-12-09 10:01:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -62,6 +62,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
    INNER JOIN pledges
    ON projects.id = pledges.project_id 
    WHERE projects.category = \"book\"
-   GROUP BY projects.categroy
+   GROUP BY projects.category
   "
 end
