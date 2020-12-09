60ab59585fc507109d8168f5a81d507e0a12018b
Michael Fuget <michaelfuget@gmail.com>
2020-12-09 09:36:01 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,17 +3,17 @@ CREATE TABLE projects (
   funding_goal INTEGER,
   start_date DATE,
   end_date DATE
-)
+);
 
 CREATE TABLE users (
   id INTEGER PRIMARY_KEY,
   name TEXT,
   age INTEGER
-)
+);
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY_KEY,
   amount INTEGER,
   user_id INTEGER,
   project_id INTEGER
-)
+);
